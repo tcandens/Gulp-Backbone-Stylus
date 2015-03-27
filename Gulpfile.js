b7c73451566a9a629685b3f171d8655804391ed8
tcandens <joey.thies@gmail.com>
2015-03-26 22:23:38 -0700

    Gulpfile and scaffolding Rough but working environment to start a Backbone.js project

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,30 @@
 (function() {
   'use strict';
   var gulp = require('gulp'),
-      usemin = require('gulp-usemin');
+      usemin = require('gulp-usemin'),
+      stylus = require('gulp-stylus'),
+      bs = require('browser-sync'),
+      del = require('del');
 
+  // Replace development versions of assets and move into build directory
   gulp.task("usemin", function() {
-    gulp.src('app/index.html')
+    return gulp.src('app/index.html')
       .pipe(usemin())
       .pipe(gulp.dest('build'))
   });
 
+  // Deletes entire build directory
+  gulp.task('clean:build', function() {
+    return del('build');
+  });
+
+  // Compiles stylus stylesheets into app/css/main.css
+  gulp.task('stylus:dev', function() {
+    return gulp.src('app/stylus/main.styl')
+      .pipe(stylus())
+      .pipe(gulp.dest('app/css/'));
+  });
+
+  
 
 })();
